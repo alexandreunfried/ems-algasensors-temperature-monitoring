1c389aac238b3a3d265a34e01e29debb1499fb58
Alexandre Henrique Unfried <alexandreunfried@gmail.com>
2025-07-02 15:46:17 -0300

    Implementação até aula 12.12. Publicando mensagens

---
+++ src/main/java/com/unfried/algasensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMQConfig.java
@@ -0,0 +1,33 @@
+package com.unfried.algasensors.temperature.monitoring.infrastructure.rabbitmq;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+	public static final String FANOUT_EXCHANGE_NAME = "temperature-processing.temperature-received.v1.e";
+	public static final String QUEUE_NAME = "temperature-monitoring.process-temperature.v1.q";
+
+	@Bean
+	public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+		return new RabbitAdmin(connectionFactory);
+	}
+
+	@Bean
+	public Queue queue() {
+		return QueueBuilder.durable(QUEUE_NAME).build();
+	}
+
+	public FanoutExchange exchange() {
+		return ExchangeBuilder.fanoutExchange(FANOUT_EXCHANGE_NAME).build();
+	}
+
+	@Bean
+	public Binding binding() {
+		return BindingBuilder.bind(queue()).to(exchange());
+	}
+}
